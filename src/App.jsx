30986db48d1907d5da257894979d479796ee6a98
KaustubhSiriki <kaustubh.siriki@gmail.com>
2024-07-09 21:53:16 -0400

    header/navbar component done
    got the header/navbar fully working and it works for all device types

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import ButtonGradient from './assets/svg/ButtonGradient'
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+import Header from "./components/Header";
 
 const App = () => {
-  return(
+  return (
     <>
-     <h1 className="text-3xl font-bold underline">Hello world!</h1>
-     <ButtonGradient />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+      </div>
+      <ButtonGradient />
     </>
   );
-}
+};
 
 export default App;
